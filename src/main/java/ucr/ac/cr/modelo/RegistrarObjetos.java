b27be43b25937729a0def9f9e03974f876e6dcc6
Usuario <Usuario@SPLab1-23>
2024-03-14 15:42:56 -0600

    ssss

---
+++ src/main/java/ucr/ac/cr/modelo/RegistrarObjetos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucr.ac.cr.modelo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class RegistrarObjetos 
+{
+    //Esto es una prueba 
+}
